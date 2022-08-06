8c8c5455dc00a35c50f79b8599c865113a4ca79b
Christian Mina <sinsimi56@gmail.com>
2022-08-07 01:19:20 +0800

    Add transpose variable and matrix multiplication.

--- Linear/ETL.cpp
+++ Linear/ETL.cpp
@@ -11,3 +11,17 @@ int main(int argc, char **argv){
     Eigen: :MatrixXd m1(2,2);
     m1 << 1,2,3,4;
     std:: cout << "\n m1 = \n1" << m1 << std::endl;
+
+    // m1 = m1.transpose();
+    m1.transposeInPlace().eval();
+    std:: cout << "\n m1 = \n1" << m1 << std::endl;
+
+    //matrix multiplication
+    Eigen:: MatrixXd m2 (2,2);
+    m2 << 5,6,8,9;
+
+    Eigen:: MatrixXd m3 (3,2);
+    m3 << 1,2,3,4,5,6;
+
+    Eigen:: MatrixXd m5 (2,2);
+    m3 << 1,2,3,4;
