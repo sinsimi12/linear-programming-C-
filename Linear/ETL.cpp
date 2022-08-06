91eba1ed2e0f97aa1a8f761773c6bd9d360eede5
Christian Mina <sinsimi56@gmail.com>
2022-08-07 01:18:50 +0800

    Add matrix transpose function.

--- Linear/ETL.cpp
+++ Linear/ETL.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
-#include <vector>
-#include <stdlib.h>
-#include <cmath>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+int main(int argc, char **argv){
+
+
+     // transposing the matrix or matrix transposition
+    Eigen: :MatrixXd m1(2,2);
+    m1 << 1,2,3,4;
+    std:: cout << "\n m1 = \n1" << m1 << std::endl;
